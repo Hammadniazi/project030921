fc6928e5b1d1ec37eb2e7fcf62293a4eae06cb0b
Hammadniazi <66183738+Hammadniazi@users.noreply.github.com>
2021-03-09 01:34:21 +0100

    again

--- src/App.js
+++ src/App.js
@@ -3,24 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Change done by Mr. Hammad
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World.Hammad is here know.
     </div>
   );
 }
